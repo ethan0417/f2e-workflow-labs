42e734e94840b4c65f5c6f3d3c629eeefa29b5c5
Ethan <ethan.mail0417@gmail.com>
2015-09-05 15:15:05 +0800

    請依序執行 mytask2, mytask1, default 工作

--- gulpfile.js
+++ gulpfile.js
@@ -13,3 +13,20 @@ gulp.task('mystask2', function(cb){
   console.log('task 2')
   cb();
 })
+
+/**
+ *  請依序執行 mytask2, mytask1, default 工作
+ */
+// gulp.task('default',['mystask1'], function(){
+//   console.log('my default task')
+// });
+
+// gulp.task('mystask1', ['mystask2'], function(cb){
+//   console.log('task 1')
+//   cb();
+// })
+
+// gulp.task('mystask2', function(cb){
+//   console.log('task 2')
+//   cb();
+// })
