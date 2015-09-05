2d671353513608d10f75bc2b7506a8014141e175
Ethan <ethan.mail0417@gmail.com>
2015-09-05 14:58:26 +0800

    Add gulp inject task function.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
 
-gulp.task('default', function(){
+gulp.task('default',['mystask1', 'mystask2'], function(){
   console.log('my default task')
 });
+
+gulp.task('mystask1', function(cb){
+  console.log('task 1')
+  cb();
+})
+
+gulp.task('mystask2', function(cb){
+  console.log('task 2')
+  cb();
+})
