1a49f944ded6b3fb556886f54d871bda68c84cea
Ethan <ethan.mail0417@gmail.com>
2015-09-05 15:25:08 +0800

    Add gulp.src and gulp.dest demo function.

--- gulpfile.js
+++ gulpfile.js
@@ -30,3 +30,15 @@ gulp.task('mystask2', function(cb){
 //   console.log('task 2')
 //   cb();
 // })
+
+// 這個dest出來的目錄，會從bootstrap以下開始輸出到output1
+gulp.task('output1', function(){
+  gulp.src('./assets/vendor/bootstrap/**/*.js')
+    .pipe(gulp.dest('output1'))
+})
+
+// 這個dest出來的目錄，會從 assets/vendor 以下開始輸出到output2
+gulp.task('output2', function(){
+  gulp.src('./assets/vendor/bootstrap/**/*.js', {base:'assets/vendor'})
+    .pipe(gulp.dest('output2'))
+})
